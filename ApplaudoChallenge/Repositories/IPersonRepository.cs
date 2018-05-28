b3bfaabc8385eff06b1b5111429effec55cc7754
Caleb Flores <kleb.13@gmail.com>
2018-05-27 21:52:16 -0600

    Add repository interface

---
+++ ApplaudoChallenge/Repositories/IPersonRepository.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ApplaudoChallenge.Models;
+
+namespace ApplaudoChallenge.Repositories
+{
+    public interface IPersonRepository
+    {
+        IEnumerable<Person> All();
+        Person FindById(int id);
+        Person Add(Person person);
+        Person Update(Person person);
+        void Delete(int id);
+    }
+}
