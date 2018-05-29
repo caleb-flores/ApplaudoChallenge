85758efddbf3ad874c955bbcc0b8da15e51a2310
Caleb Flores <kleb.13@gmail.com>
2018-05-28 21:25:13 -0600

    Add fake data

--- ApplaudoChallenge/Startup.cs
+++ ApplaudoChallenge/Startup.cs
@@ -1,11 +1,13 @@
 ﻿using ApplaudoChallenge.ContractResolver;
 using ApplaudoChallenge.Data;
 using ApplaudoChallenge.Repositories;
+using Bogus;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Person = ApplaudoChallenge.Models.Person;
 
 namespace ApplaudoChallenge
 {
@@ -33,14 +35,26 @@ namespace ApplaudoChallenge
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env,ApplicationDbContext context)
         {
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
             }
-
+            GenerateFakeData(context);
             app.UseMvc();
+        }
+
+        void GenerateFakeData(ApplicationDbContext context)
+        {
+            var personFaker = new Faker<Person>()
+                .RuleFor(p => p.FirstName, f => f.Person.FirstName)
+                .RuleFor(p => p.LastName, f=> f.Person.LastName)
+                .RuleFor(p => p.Disabled, f => f.Random.Bool());
+
+            var persons = personFaker.Generate(100);
+            context.AddRange(persons);
+            context.SaveChanges();
         }
     }
 }
